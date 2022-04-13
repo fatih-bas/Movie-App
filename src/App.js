c8c853eb491a309be2bd3bc982ac3b4d5bfea2b2
fatih-bas <fatihbas1@gmail.com>
2022-04-13 23:59:41 +0300

    added movie filter

--- src/App.js
+++ src/App.js
@@ -1,71 +1,59 @@
-import React, {useEffect, useState} from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
 import Movie from "./components/Movie";
-import SearchButton from "./components/Button"
- 
 
-const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3c517892331a735aa3dae9ab8db39faa&page=1";
-
-const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=3c517892331a735aa3dae9ab8db39faa&query=";
-
-
+const API_KEY = "3c517892331a735aa3dae9ab8db39faa";
+const FEATURED_API = `https://api.themoviedb.org/4/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}&page=1}`;
+const SEARCH_API = `https://api.themoviedb.org/4/search/movie?&api_key=${API_KEY}&query=`;
 function App() {
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
-
-  useEffect(() => {
-    getMovies(FEATURED_API)
-  }, []);
 
   const getMovies = (API) => {
     fetch(API)
       .then((res) => res.json())
       .then((data) => {
+        console.log(data.page);
         setMovies(data.results);
       });
-  }
+  };
+
+  useEffect(() => {
+    getMovies(FEATURED_API);
+  }, []);
 
   const handleOnSubmit = (e) => {
     e.preventDefault();
-
-    if(searchTerm) {
+    if (searchTerm) {
       getMovies(SEARCH_API + searchTerm);
-      
       setSearchTerm("");
     }
-  }
-
-  const handleClick = (e) => {
-    e.preventDefault();
-    getMovies(SEARCH_API + searchTerm)
-  } 
+  };
 
   const handleOnChange = (e) => {
     setSearchTerm(e.target.value);
-  }
- 
+  };
+
   return (
     <>
       <header>
         <form onSubmit={handleOnSubmit}>
-          <input 
-            type="search" 
-            className="search" 
-            placeholder='Search...'
-            value={searchTerm} 
+          <input
+            type="search"
+            className="search"
+            placeholder="Search"
+            value={searchTerm}
             onChange={handleOnChange}
           />
-         
         </form>
-        <SearchButton handleClick={handleClick}/> 
       </header>
-   
       <div className="movie-container">
-        
-        {
-          movies.length > 0 && movies.map(movie => 
-          <Movie  key={movie.id} {...movie}/>)
-        } 
+        {movies
+          .filter((movie) =>
+            movie.title.toLowerCase().includes(searchTerm.toLowerCase())
+          )
+          .map((movie) => (
+            <Movie key={movie.id} {...movie} />
+          ))}
       </div>
     </>
   );
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,43 +1,29 @@
-import React from 'react';
+import React from "react";
 
-const setVoteClass = (vote) => {
-  if(vote >= 8) {
-    return "green";
-  } else if (vote >=6){
-    return "orange";
-  } else {
-    return "red"
-  }
-}
+const blankPoster =
+  "https://images.unsplash.com/photo-1440404653325-ab127d49abc1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80";
+
+const IMG_API = "https://image.tmdb.org/t/p/w500/";
 
 const Movie = (results) => {
-  const IMG_API = "https://image.tmdb.org/t/p/w500/";
   return (
-
-      <div className="movie">
-        <img 
-          src={
-            results.poster_path
-            ? IMG_API + results.poster_path
-            : "https://images.unsplash.com/photo-1440404653325-ab127d49abc1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-          } 
-          alt={results.title}/>
-        <div className="movie-info">
-          <h3>{results.title}</h3>
-          <span 
-            className={`tag ${setVoteClass(results.vote_average)}`}>{results.vote_average}</span>
-        </div>
-        <div className="movie-overview">
-        <h3>Overview</h3>
-          <p>{results.overview}</p>
-        </div>
-          
-  
+    <div className="movie">
+      <img
+        src={results.poster_path ? IMG_API + results.poster_path : blankPoster}
+        alt={results.title}
+      />
+      <div className="movie-info">
+        <h3>{results.title}</h3>
+        <span className={`tag ${results.vote_average}`}>
+          {results.vote_average}
+        </span>
       </div>
-        
-    
-    
-  )
+      <div className="movie-overview">
+        <h3>Overview</h3>
+        <p>{results.overview}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Movie;
